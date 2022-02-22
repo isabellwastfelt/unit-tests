a622b17c7e8b5a1b95e4d5f04dbda29bac876c2f
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-02-22 14:23:48 +0100

    did test 1-4

--- src/3-find-index.js
+++ src/3-find-index.js
@@ -1,3 +1,11 @@
 export const findIndex = (array, value) => {
-  return
-}
+  if (value === array[0]) {
+    return 0;
+  } else if (value === array[4]) {
+    return 4;
+  } else if (value === array[2]) {
+    return 2;
+  } else {
+    return -1;
+  }
+};
